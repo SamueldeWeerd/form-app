20c61c17a62b557a5e77b2ebd52649ea221f839c
Samuel <samueldeweerd@gmail.com>
2024-10-08 14:40:30 +0200

    Implemented angular form template for form submission and validation.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  onSubmitForm(formData: NgForm){
+
+    if (formData.form.invalid) {
+      return
+    }
+
+    const enteredEmail : string = formData.form.value.enteredEmail;
+    const enteredPassword : string = formData.form.value.enteredPassword;
+
+    console.log(enteredEmail, enteredPassword);
+  }
+}
