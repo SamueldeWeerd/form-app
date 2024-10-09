8c6433876519fbe1d7462e81b05cc8634f4fafec
Samuel <samueldeweerd@gmail.com>
2024-10-09 10:53:00 +0200

    Added more form controls. Email is stored in browser and repopulates form after refresh.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { afterNextRender, Component, DestroyRef, inject, viewChild, ViewChild } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,6 +10,38 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private form = viewChild.required<NgForm>('emailForm');
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    afterNextRender(
+
+      () => {
+        const savedForm = window.localStorage.getItem('saved-login-form');
+
+        if (savedForm){
+          const loadedFormData = JSON.parse(savedForm);
+          const loadedEmail = loadedFormData.email;
+          setTimeout(
+            () => this.form().controls['email'].setValue(loadedEmail), 
+            1
+            )
+        
+        }
+
+        const subscription = this.form().valueChanges?.pipe(
+          debounceTime(500)
+        )
+        .subscribe(
+          {next: (value) => window.localStorage.setItem(
+            'saved-login-form', 
+            JSON.stringify({email: value.email}))}
+        );
+        this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+      }
+    );
+  }
+
   onSubmitForm(formData: NgForm){
 
     if (formData.form.invalid) {
@@ -19,5 +52,7 @@ export class LoginComponent {
     const enteredPassword : string = formData.form.value.enteredPassword;
 
     console.log(enteredEmail, enteredPassword);
+
+    formData.form.reset();
   }
 }
