9f37bee86cbfb97990f27bc14a40ac0e4a3f70f8
Samuel <samueldeweerd@gmail.com>
2024-10-09 13:09:09 +0200

    Created dummy validator for email, check if unique. Has to be updated with HTTP request in future.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,23 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { debounceTime, of } from 'rxjs';
+
+function mustContainQuestionMark(control: AbstractControl) {
+  if (control.value.includes('?')) {
+    return null;
+  } else {
+    return { doesNotContainQuestionMark: true};
+  }
+}
+
+function emailIsUnique(control: AbstractControl) {
+  if (control.value !== 'test@example.com'){
+    return of(null)
+  } else {
+    return of({notUniqueEmail: true})
+  }
+
+}
 
 @Component({
   selector: 'app-login',
@@ -8,17 +26,27 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   form = new FormGroup(
     {
       email: new FormControl('', {
-        validators: [Validators.required, Validators.email]
+        validators: [Validators.required, Validators.email],
+        asyncValidators: [emailIsUnique]
       }),
       password: new FormControl('', {
-        validators: [Validators.required, Validators.minLength(6)]
+        validators: [Validators.required, Validators.minLength(6), mustContainQuestionMark]
       })
      }
   );
+
+  ngOnInit(): void {
+    this.form.valueChanges.pipe(
+      debounceTime(500)
+    )
+    .subscribe({
+      next: (value) => window.localStorage.setItem('saved-login-form', JSON.stringify({email: value.email}))
+    });
+  }
 
   get emailIsInvalid(): boolean {
     return this.form.controls.email.touched && this.form.controls.email.dirty && this.form.controls.email.invalid
